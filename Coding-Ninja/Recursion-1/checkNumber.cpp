eeed2a5abe35792aef2874130da9ed797a197d1d
Khusharth <khusharth19@gmail.com>
2020-11-19 15:20:11 +0530

    Adding question: Check if number present

---
+++ Coding-Ninja/Recursion-1/checkNumber.cpp
@@ -0,0 +1,81 @@
+/*
+Check Number
+
+Given an array of length N and an integer x, you need to find if x is present in the array or not. Return true or false.
+Do this recursively.
+Input Format :
+
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Line 3 : Integer x
+
+Output Format :
+
+'true' or 'false'
+
+Constraints :
+
+1 <= N <= 10^3
+
+Sample Input 1 :
+
+3
+9 8 10
+8
+
+Sample Output 1 :
+
+true
+
+Sample Input 2 :
+
+3
+9 8 10
+2
+
+Sample Output 2 :
+
+false
+
+*/
+
+#include<iostream>
+using namespace std;
+bool checkNumber(int arr[], int size, int x) {
+	// BASE CASE
+    if (size == 0) {
+        return false;
+    }
+    
+    // Small step
+    if (arr[0] == x) return true;
+    
+    // Recursive step
+    bool isNumPresentInSmallArr = checkNumber(arr + 1, size - 1, x);
+    
+    return isNumPresentInSmallArr;
+}
+
+int main() {
+	int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    int x;
+    
+    cin >> x;
+    
+    if(checkNumber(input, n, x)) {
+        cout << "true" << endl;
+    }
+    else {
+        cout << "false" << endl;
+    }
+    
+	return 0;
+}
